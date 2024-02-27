e89780c45bb5d13917d7d918b3ed8d3cd3171265
HeverttonNoliveira <oliveirahn.nunes@gmail.com>
2024-02-27 11:23:22 -0300

    Aula sobre View e Safeareaview

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { View } from "react-native";
+
+// Componentes
+import Parte0, { Parte1, Parte2 } from './components/Parte1';
+
+function App() {
+    return (
+        <View>
+            <Parte0 />
+            <Parte1 />
+            <Parte2 />
+        </View>
+    )
+}
+
+export default App;
---
+++ src/components/Parte1.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Text } from "react-native";
+
+export default function Parte0() {
+    return <Text>Parte 0</Text>
+}
+export function Parte1() {
+    return <Text style>Segundo</Text>
+}
+export function Parte2() {
+    return <Text>Olá mundo</Text>
+}
+
+
+
+
